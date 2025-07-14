cdb2c1a6599d9e473e5302ad7c807024e68bd108
saksham sharma <sakshams982@gmail.com>
2025-07-14 16:26:14 +0545

    Implemented custom user model and manager

--- assignit/assignit/settings.py
+++ assignit/assignit/settings.py
@@ -17,10 +17,8 @@ from pathlib import Path
 
 load_dotenv()
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -32,7 +30,6 @@ SECRET_KEY = 'django-insecure-6d7ilpnrw$%(+d-+j82!e^o)ov1iwtz^x+h=z*b#7@eo394d@5
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'assignit.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
@@ -91,6 +87,8 @@ DATABASES = {
         'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
     }
 }
+
+AUTH_USER_MODEL = 'user.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -110,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -121,7 +118,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
--- assignit/user/models.py
+++ assignit/user/models.py
@@ -2,14 +2,21 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MinLengthValidator
 
+from user.managers import UserManager
+
+
 class User(AbstractUser):
     username = models.CharField(max_length=128, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128, validators=[MinLengthValidator(8)])
 
     image_url = models.URLField(blank=True, null=True)
-    otp_code = models.CharField(max_length=4, unique=True, blank=True, null=True)
+    otp_code = models.CharField(max_length=4, blank=True, null=True)
     otp_expiry = models.DateTimeField(blank=True, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
