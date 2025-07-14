0dd5d3ba3dca028ece48ad4915c61fa708529e9c
saksham sharma <sakshams982@gmail.com>
2025-07-14 14:01:02 +0545

    Initialized User app with custom user model

---
+++ assignit/user/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+
+class User(AbstractUser):
+    username = models.CharField(max_length=128, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128, validators=[MinLengthValidator(8)])
+
+    image_url = models.URLField(blank=True, null=True)
+    otp_code = models.CharField(max_length=4, unique=True, blank=True, null=True)
+    otp_expiry = models.DateTimeField(blank=True, null=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
