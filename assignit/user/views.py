0faa12c3afbe258fbc3459967dcc7b414ad4b838
saksham sharma <sakshams982@gmail.com>
2025-07-16 11:44:10 +0545

    - Implemented signup API view

--- assignit/user/views.py
+++ assignit/user/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from user.serializers import UserSignUpSerializer
+
+
+class UserSignUpAPIView(APIView):
+    permission_classes = [AllowAny,]
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = RefreshToken.for_user(user)
+        response_data = {
+                'refresh': str(token),
+                'access': str(token.access_token),
+                'user':{
+                    'id': user.id,
+                    'username': user.username,
+                    'email': user.email,
+                    'imageUrl': user.image_url,
+                }}
+        return Response(response_data, status=status.HTTP_201_CREATED)
